0462ed673e4c34eb4932e1e8c411a2b3762987b5
kaanayhan <kaanayhan730@gmail.com>
2026-01-11 12:12:34 +0300

    Detaylandırma

--- JavaOdev.java
+++ JavaOdev.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class JavaOdev { // Türkçe karakter hatasını önlemek için 'O' kullanıldı
+public class JavaOdev {
 
     public static void main(String[] args) {
         Scanner oku = new Scanner(System.in);
@@ -9,14 +9,14 @@ public class JavaOdev { // Türkçe karakter hatasını önlemek için 'O' kulla
 
         String[] takimlar = new String[4];
         boolean takimlarGirildi = false;
-        
+
         // Skorlar: [Hafta][4 Skor (H1, D1, H2, D2)]
         int[][] haftalikSkorlar = new int[6][4];
-        
+
         // Fikstür: Her hafta oynayacak takımların indisleri
         int[][] fikstur = {
-            {0, 3, 1, 2}, {0, 2, 3, 1}, {0, 1, 2, 3}, // 1. Devre
-            {3, 0, 2, 1}, {2, 0, 1, 3}, {1, 0, 3, 2}  // 2. Devre
+                { 0, 3, 1, 2 }, { 0, 2, 3, 1 }, { 0, 1, 2, 3 }, // 1. Devre
+                { 3, 0, 2, 1 }, { 2, 0, 1, 3 }, { 1, 0, 3, 2 } // 2. Devre
         };
 
         // Skorları -1 (oynanmadı) ile başlat
@@ -27,17 +27,18 @@ public class JavaOdev { // Türkçe karakter hatasını önlemek için 'O' kulla
         }
 
         while (true) {
-            System.out.println("------    ANA MENÜ    ------");
+            System.out.println("\n------    ANA MENÜ    ------");
             System.out.println("1. Takım İsimlerini Gir");
             System.out.println("2. Fikstürü Görüntüle");
             System.out.println("3. Haftalık Skorları Rastgele Ata");
-            System.out.println("4. Puan Durumunu Göster");
+            System.out.println("4. Puan Durumunu Göster (Sıralı)");
             System.out.println("5. Çıkış");
             System.out.print("Seçiminiz: ");
-            
+
             char secim = oku.next().charAt(0);
 
-            if (secim == '5') break;
+            if (secim == '5')
+                break;
 
             if (secim >= '2' && secim <= '4' && !takimlarGirildi) {
                 System.out.println("Önce takımları girmelisiniz.");
@@ -54,63 +55,113 @@ public class JavaOdev { // Türkçe karakter hatasını önlemek için 'O' kulla
                     break;
 
                 case '2':
-                    System.out.println("--- SEZON FİKSTÜRÜ ---");
+                    System.out.println("\n--- SEZON FİKSTÜRÜ ---");
                     for (int i = 0; i < 6; i++) {
-                        if (i == 0) System.out.println("[1. DEVRE]");
-                        if (i == 3) System.out.println("[2. DEVRE]");
-                        
-                        System.out.printf("%d. HAFTA: %s - %s | %s - %s", 
-                            (i + 1), takimlar[fikstur[i][0]], takimlar[fikstur[i][1]], 
-                            takimlar[fikstur[i][2]], takimlar[fikstur[i][3]]);
+                        if (i == 0)
+                            System.out.println("[1. DEVRE]");
+                        if (i == 3)
+                            System.out.println("\n[2. DEVRE]");
+
+                        System.out.printf("%d. HAFTA: %s - %s | %s - %s%n",
+                                (i + 1), takimlar[fikstur[i][0]], takimlar[fikstur[i][1]],
+                                takimlar[fikstur[i][2]], takimlar[fikstur[i][3]]);
                     }
                     break;
 
                 case '3':
-                    System.out.println("--- SKORLAR ATANIYOR ---");
+                    System.out.println("\n--- SKORLAR ATANIYOR ---");
                     for (int i = 0; i < 6; i++) {
                         if (haftalikSkorlar[i][0] == -1) {
-                            for (int j = 0; j < 4; j++) haftalikSkorlar[i][j] = rastgele.nextInt(5);
+                            for (int j = 0; j < 4; j++)
+                                haftalikSkorlar[i][j] = rastgele.nextInt(5);
                         }
-                        // Oynanan maçları yazdır
-                        System.out.printf("%d. Hafta: %s %d-%d %s | %s %d-%d %s",
-                            (i+1), takimlar[fikstur[i][0]], haftalikSkorlar[i][0], haftalikSkorlar[i][1], takimlar[fikstur[i][1]],
-                            takimlar[fikstur[i][2]], haftalikSkorlar[i][2], haftalikSkorlar[i][3], takimlar[fikstur[i][3]]);
+                        System.out.printf("%d. Hafta: %s %d-%d %s | %s %d-%d %s%n",
+                                (i + 1), takimlar[fikstur[i][0]], haftalikSkorlar[i][0], haftalikSkorlar[i][1],
+                                takimlar[fikstur[i][1]],
+                                takimlar[fikstur[i][2]], haftalikSkorlar[i][2], haftalikSkorlar[i][3],
+                                takimlar[fikstur[i][3]]);
                     }
                     break;
 
                 case '4':
-                    int[] G = new int[4], B = new int[4], M = new int[4], P = new int[4];
-                    int[] AG = new int[4], YG = new int[4], OM = new int[4];
+                    // Değişken isimleri detaylandırıldı
+                    int[] galibiyet = new int[4];
+                    int[] beraberlik = new int[4];
+                    int[] yenilgi = new int[4];
+                    int[] puan = new int[4];
+                    int[] atilanGol = new int[4];
+                    int[] yenilenGol = new int[4];
+                    int[] oynananMac = new int[4];
+                    int[] averaj = new int[4];
+                    int[] siraliIndisler = { 0, 1, 2, 3 };
 
+                    // İstatistikleri Hesapla
                     for (int h = 0; h < 6; h++) {
-                        if (haftalikSkorlar[h][0] == -1) continue;
-                        
-                        // Her haftadaki 2 maçı döngüyle işle (Kod tasarrufu sağlar)
+                        if (haftalikSkorlar[h][0] == -1)
+                            continue;
+
                         for (int mac = 0; mac < 2; mac++) {
                             int evIndis = (mac == 0) ? 0 : 2;
                             int depIndis = (mac == 0) ? 1 : 3;
-                            
+
                             int ev = fikstur[h][evIndis];
                             int dep = fikstur[h][depIndis];
                             int evSkor = haftalikSkorlar[h][evIndis];
                             int depSkor = haftalikSkorlar[h][depIndis];
 
-                            OM[ev]++; OM[dep]++;
-                            AG[ev] += evSkor; YG[ev] += depSkor;
-                            AG[dep] += depSkor; YG[dep] += evSkor;
+                            oynananMac[ev]++;
+                            oynananMac[dep]++;
+                            atilanGol[ev] += evSkor;
+                            yenilenGol[ev] += depSkor;
+                            atilanGol[dep] += depSkor;
+                            yenilenGol[dep] += evSkor;
 
-                            if (evSkor > depSkor) { P[ev] += 3; G[ev]++; M[dep]++; }
-                            else if (depSkor > evSkor) { P[dep] += 3; G[dep]++; M[ev]++; }
-                            else { P[ev]++; P[dep]++; B[ev]++; B[dep]++; }
+                            if (evSkor > depSkor) {
+                                puan[ev] += 3;
+                                galibiyet[ev]++;
+                                yenilgi[dep]++;
+                            } else if (depSkor > evSkor) {
+                                puan[dep] += 3;
+                                galibiyet[dep]++;
+                                yenilgi[ev]++;
+                            } else {
+                                puan[ev]++;
+                                puan[dep]++;
+                                beraberlik[ev]++;
+                                beraberlik[dep]++;
+                            }
                         }
                     }
 
-                    System.out.printf("%-15s %-3s %-3s %-3s %-3s %-3s %-3s %-3s %-3s", 
-                                      "Takım", "OM", "G", "B", "M", "AG", "YG", "AV", "P");
-                    System.out.println("---------------------------------------------------------");
+                    for (int i = 0; i < 4; i++)
+                        averaj[i] = atilanGol[i] - yenilenGol[i];
+
+                    // Sıralama (Puan ve Averaj kontrolü ile)
+                    for (int i = 0; i < 4 - 1; i++) {
+                        for (int j = 0; j < 4 - i - 1; j++) {
+                            int t1 = siraliIndisler[j];
+                            int t2 = siraliIndisler[j + 1];
+
+                            if (puan[t1] < puan[t2] || (puan[t1] == puan[t2] && averaj[t1] < averaj[t2])) {
+                                int gecici = siraliIndisler[j];
+                                siraliIndisler[j] = siraliIndisler[j + 1];
+                                siraliIndisler[j + 1] = gecici;
+                            }
+                        }
+                    }
+
+                    System.out.println("\n--- PUAN DURUMU (SIRALI) ---");
+                    System.out.printf("%-4s %-15s %-3s %-10s %-10s %-10s %-7s %-7s %-7s %-5s%n",
+                            "Sıra", "Takım", "OynananMaç", "Galibiyet", "Beraberlik", "Yenilgi", "Atılan", "Yenilen", "Averaj",
+                            "Puan");
+                    System.out.println(
+                            "-----------------------------------------------------------------------------------------");
+
                     for (int i = 0; i < 4; i++) {
-                        System.out.printf("%-15s %-3d %-3d %-3d %-3d %-3d %-3d %-3d %-3d",
-                            takimlar[i], OM[i], G[i], B[i], M[i], AG[i], YG[i], (AG[i]-YG[i]), P[i]);
+                        int idx = siraliIndisler[i];
+                        System.out.printf("%-4d %-15s %-3d %-10d %-10d %-10d %-7d %-7d %-7d %-5d%n",
+                                (i + 1), takimlar[idx], oynananMac[idx], galibiyet[idx], beraberlik[idx], yenilgi[idx],
+                                atilanGol[idx], yenilenGol[idx], averaj[idx], puan[idx]);
                     }
                     break;
 
